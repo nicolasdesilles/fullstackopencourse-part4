4b6b0184ac489732ea0e617fe0336890ff21b6c7
Nicolas Désilles <nicolas@ndlabs.fr>
2025-02-15 19:25:23 +0100

    Finished Exercise 4.1

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log('hey')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI;
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
