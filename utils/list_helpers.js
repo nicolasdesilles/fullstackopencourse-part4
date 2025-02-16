87eec563fdfb4f432fd2a9e36ab8e8a28bb530d6
Nicolas Désilles <nicolas@ndlabs.fr>
2025-02-16 14:47:23 +0100

    Finished Exercise 4.4

--- utils/list_helpers.js
+++ utils/list_helpers.js
@@ -1,7 +1,18 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  var amountOfLikes = 0
+  for (let i = 0; i < blogs.length; i++) {
+    const blog = blogs[i]
+    amountOfLikes += blog.likes
+  }
+  return amountOfLikes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
