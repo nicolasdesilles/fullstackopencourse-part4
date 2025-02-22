7b770a36a706d7a81dec08e5b5182e8536d300f9
Nicolas Désilles <nicolas@ndlabs.fr>
2025-02-22 22:02:45 +0100

    Finished Exercise 4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,11 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({})
+    .populate('user', { username: 1, name: 1, id: 1})
   response.json(blogs)
 })
 
@@ -17,8 +20,26 @@ blogsRouter.get('/:id', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+
+  const usersInDb = await User.find({})
+  if (usersInDb.length === 0) {
+    response.status(400).json({ error: 'no users in db, please add at least one user before adding blogs'})
+  }
+  const user = usersInDb[0]
+
+  const blog = new Blog({
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes,
+    user: user.id
+  })
+
   const result = await blog.save()
+
+  user.blogs = user.blogs.concat(result._id)
+  await user.save()
+
   response.status(201).json(result)
 })
 
